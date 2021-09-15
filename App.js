c3ddf63bdf36fae162f1b980cf7a22bd493bc368
n9leja02 <n9leja01@students.oamk.fi>
2021-09-15 18:06:19 +0300

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Platform, Pressable, StyleSheet, Text, View } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker'
 
 export default function App() {
+  const [date, setDate] = useState(new Date());
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    setShow(false);
+    const currentDate = selectedDate || date;
+    setDate(currentDate);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {show && Platform.OS === 'ios' && (
+        <DateTimePicker
+          style={{ width: 320 }}
+          mode={'date'}
+          display="inline"
+          value={date}
+          onChange={onChange}
+        />
+      )}
+      {show && Platform.OS === 'android' && (
+        <DateTimePicker          
+          mode={'date'}
+          display="default"
+          value={date}
+          onChange={onChange}
+        />
+      )}
     </View>
   );
 }
@@ -15,6 +39,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    marginTop: 50,
     alignItems: 'center',
     justifyContent: 'center',
   },
